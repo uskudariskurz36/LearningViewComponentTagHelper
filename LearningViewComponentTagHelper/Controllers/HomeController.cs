33b6207b59c8ab829c67f6e56540857b20a4de8f
Murat Başeren <uskudar.iskur.z36@gmail.com>
2022-12-21 09:34:44 +0300

    Add project files.

---
+++ LearningViewComponentTagHelper/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using LearningViewComponentTagHelper.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LearningViewComponentTagHelper.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        public IActionResult List()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LearningViewComponentTagHelper/Models/AddressInfoPartialModel.cs
@@ -0,0 +1,26 @@
+﻿namespace LearningViewComponentTagHelper.Models
+{
+    public class AddressInfoPartialModel
+    {
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string EMail { get; set; }
+        public string Title { get; set; }
+        public Colors PanelColor { get; set; }
+    }
+
+    public enum Colors
+    {
+        none,
+        danger,
+        dark,
+        info,
+        light,
+        primary,
+        secondary,
+        success,
+        warning,
+    }
+}
